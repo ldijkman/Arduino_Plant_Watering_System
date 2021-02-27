f3c88b0470a625f4c917a491c5f4448ad77840ba
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-02-27 16:02:17 +0100

    Add files via upload

--- Menu_test_arduino_plant_watering.ino
+++ Menu_test_arduino_plant_watering.ino
@@ -2,6 +2,7 @@
 // working on     SD card Logging for mega 2560 board (not enough progmem space on nano /uno)
 // working on     creates a comma seperated log file datestamp .txt on SD card each day
 // on a mega2560 allmost everything / every change is logged to sdcard
+//but how do a visualize such a LOG file online or in a spreadsheet editor???
 
 // Arduino Advanced Automated Plant Watering System, StandAlone, Low Cost, Low Power Consumption
 // Copyright 2021 Dirk Luberth Dijkman Bangert 30 1619GJ Andijk The Netherlands
